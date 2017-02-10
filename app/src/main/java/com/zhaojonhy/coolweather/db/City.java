1f5e22ed1f3fe3d2492eb076ccdfb81043bba635
zhaojonhy <773486915@qq.com>
2017-02-10 17:44:29 +0800

    db数据库表格的实体类，存放，省、市、城镇

---
+++ app/src/main/java/com/zhaojonhy/coolweather/db/City.java
@@ -0,0 +1,45 @@
+package com.zhaojonhy.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Administrator on 2017/2/10.
+ */
+public class City extends DataSupport {
+    private int id ;
+    private String cityName ;
+    private int cityCode ;
+    private int privinceId ;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getPrivinceId() {
+        return privinceId;
+    }
+
+    public void setPrivinceId(int privinceId) {
+        this.privinceId = privinceId;
+    }
+}
