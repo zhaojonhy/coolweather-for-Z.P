ca865a24f55b50930e585c81335dfcdda9d40a82
zhaojonhy <773486915@qq.com>
2017-02-12 23:44:37 +0800

    用okHttp与服务器交互,得到全国地质的json数据并注册网络权限

--- app/src/main/java/com/zhaojonhy/coolweather/MainActivity.java
+++ app/src/main/java/com/zhaojonhy/coolweather/MainActivity.java
@@ -2,13 +2,35 @@ package com.zhaojonhy.coolweather;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.zhaojonhy.coolweather.util.HttpUtil;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+    private final static String TAG = MainActivity.class.getSimpleName() ;
+    public static String COUNTRID =  "http://files.heweather.com/china-city-list.json" ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        HttpUtil.sendOkHttpRequest(COUNTRID, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.d(TAG,"fail-->" + e.getMessage()) ;
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Log.d(TAG,"onResponse-->" + response ) ;
+            }
+        });
     }
 }
